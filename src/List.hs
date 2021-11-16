64b992ed0edfbe80fc3039591490d9e8fb20ea9d
ambroslins <ambros.lins@gmail.com>
2021-11-16 23:49:01 +0100

    sort List exports

--- src/List.hs
+++ src/List.hs
@@ -1,12 +1,24 @@
 module List
-  ( (++),
+  ( -- * Accessors
+
+    -- ** Length information
+    GHC.List.length,
+    GHC.List.null,
+
+    -- ** Indexing
     head,
     last,
-    tail,
+
+    -- ** Sublists
     init,
+    tail,
+    Data.List.take,
+    Data.List.drop,
+    Data.List.splitAt,
+    Data.List.stripPrefix,
     Data.List.uncons,
-    singleton,
-    GHC.List.null,
+
+    -- * Transformations
     Data.List.map,
     Data.List.reverse,
     Data.List.intersperse,
@@ -14,10 +26,14 @@ module List
     Data.List.transpose,
     Data.List.subsequences,
     Data.List.permutations,
+    Data.List.inits,
+    Data.List.tails,
+
+    -- * Reductions (folds)
     foldr,
     foldl',
-    GHC.List.concat,
-    GHC.List.concatMap,
+
+    -- ** Aggregate functions
     GHC.List.and,
     GHC.List.or,
     GHC.List.any,
@@ -26,41 +42,63 @@ module List
     product,
     maximum,
     minimum,
+    maximumBy,
+    minimumBy,
+
+    -- * Construction
+    empty,
+    singleton,
+    Data.List.replicate,
+
+    -- ** Unfold
+    Data.List.unfoldr,
+
+    -- ** Concatenation
+    (++),
+    GHC.List.concat,
+    GHC.List.concatMap,
+
+    -- ** Scans
+    Data.List.scanr,
     Data.List.scanl,
     Data.List.scanl',
-    Data.List.scanr,
-    Data.List.iterate,
-    Data.List.iterate',
-    Data.List.repeat,
-    Data.List.replicate,
-    Data.List.cycle,
-    Data.List.unfoldr,
-    Data.List.take,
-    Data.List.drop,
-    Data.List.splitAt,
+
+    -- * Working with predicates
+
+    -- ** Filter
+    Data.List.filter,
+    Data.List.delete,
+    Data.List.deleteBy,
+
+    -- ** Partition
+    Data.List.partition,
+    Data.List.span,
+    Data.List.break,
     Data.List.takeWhile,
     Data.List.dropWhile,
     Data.List.dropWhileEnd,
-    Data.List.span,
-    Data.List.break,
-    Data.List.stripPrefix,
     Data.List.group,
-    Data.List.inits,
-    Data.List.tails,
-    Data.List.isPrefixOf,
-    Data.List.isSuffixOf,
-    Data.List.isInfixOf,
-    Data.List.isSubsequenceOf,
+    Data.List.groupBy,
+
+    -- * Searching
     GHC.List.elem,
     GHC.List.notElem,
     Data.List.lookup,
     find,
-    Data.List.filter,
-    Data.List.partition,
+
+    -- ** Index search
     Data.List.elemIndex,
     Data.List.elemIndices,
     Data.List.findIndex,
     Data.List.findIndices,
+
+    -- * Predicates
+    Data.List.isPrefixOf,
+    Data.List.isSuffixOf,
+    Data.List.isInfixOf,
+    Data.List.isSubsequenceOf,
+
+    -- * Zipping
     Data.List.zip,
     Data.List.zip3,
     Data.List.zip4,
@@ -73,30 +111,21 @@ module List
     Data.List.zipWith5,
     Data.List.zipWith6,
     Data.List.zipWith7,
+
+    -- * Unzipping
     Data.List.unzip,
     Data.List.unzip3,
     Data.List.unzip4,
     Data.List.unzip5,
     Data.List.unzip6,
     Data.List.unzip7,
-    Data.List.nub,
-    Data.List.delete,
-    (Data.List.\\),
-    Data.List.union,
-    Data.List.intersect,
+
+    -- * Sorted lists
     Data.List.sort,
     Data.List.sortOn,
-    Data.List.insert,
-    Data.List.nubBy,
-    Data.List.deleteBy,
-    Data.List.deleteFirstsBy,
-    Data.List.unionBy,
-    Data.List.intersectBy,
-    Data.List.groupBy,
     Data.List.sortBy,
+    Data.List.insert,
     Data.List.insertBy,
-    maximumBy,
-    minimumBy,
   )
 where
 
@@ -116,6 +145,17 @@ import Prelude
 
 -- $setup
 -- >>> import Prelude (even, mod)
+
+-- | \(\mathcal{O}(1)\). Empty list.
+empty :: [a]
+empty = []
+
+-- | \(\mathcal{O}(1)\). Make a singleton list.
+--
+-- >>> singleton 1
+-- [1]
+singleton :: a -> [a]
+singleton x = [x]
 
 -- | \(\mathcal{O}(1)\). The first element of a list, or `Nothing` if empty.
 --
@@ -163,13 +203,6 @@ init (x : xs) = Just (go x xs)
   where
     go _ [] = []
     go y (z : zs) = y : go z zs
-
--- | \(\mathcal{O}(1)\). Make a singleton list.
---
--- >>> singleton 1
--- [1]
-singleton :: a -> [a]
-singleton x = [x]
 
 -- | \(\mathcal{O}(n)\). Compute the sum of all elements in the list
 --
