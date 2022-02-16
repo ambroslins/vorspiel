8d10b6a5cb4443f37c5e3cf6bab168389e523d8e
ambroslins <ambros.lins@gmail.com>
2022-02-16 21:52:33 +0100

    improve Set haddock

--- src/Set.hs
+++ src/Set.hs
@@ -5,16 +5,117 @@
 --
 -- This module exports safe functions from "Data.Set".
 module Set
-  ( module Data.Set,
+  ( -- * IntMap
+    Set,
+    null,
+    size,
+
+    -- * Construction
+    empty,
+    singleton,
+    powerSet,
+
+    -- ** From Unordered Lists
+    fromList,
+
+    -- ** From Ascending Lists
+    fromAscList,
+    fromDistinctAscList,
+
+    -- ** From Descending Lists
+    fromDescList,
+    fromDistinctDescList,
+
+    -- * Query
+    member,
+    notMember,
+
+    -- ** Lookup Ordered
+    lookupLT,
+    lookupGT,
+    lookupLE,
+    lookupGE,
+    lookupMin,
+    lookupMax,
+
+    -- ** Lookup Index
+    lookupIndex,
+
+    -- ** Subset
+    isSubsetOf,
+    isProperSubsetOf,
+
+    -- ** Disjoint
+    disjoint,
+
+    -- * Insert
+    insert,
+
+    -- * Delete
+    delete,
+    deleteMin,
+    deleteMax,
+
+    -- * Update
+    alterF,
+
+    -- * View
+    minView,
+    maxView,
+
+    -- * Combine
+
+    -- ** Union
+    union,
+    unions,
+    disjointUnion,
+
+    -- ** Difference
+    difference,
+    (\\),
+
+    -- ** Intersection
+    intersection,
+
+    -- ** Cartesian
+    cartesianProduct,
+
+    -- * Map
+    map,
+    mapMonotonic,
+
+    -- * Fold
+
+    -- ** Lazy
+    foldr,
+    foldl,
+
+    -- ** Strict
+    foldr',
+    foldl',
+
+    -- * Conversions
+    elems,
+
+    -- ** Lists
+    toList,
+    toAscList,
+    toDescList,
+
+    -- * Filter
+    filter,
+
+    -- * Partition
+    partition,
+
+    -- * Split
+    take,
+    drop,
+    split,
+    splitAt,
+    splitMember,
+    splitRoot,
   )
 where
 
-import Data.Set hiding
-  ( deleteAt,
-    deleteFindMax,
-    deleteFindMin,
-    elemAt,
-    findIndex,
-    findMax,
-    findMin,
-  )
+import Data.Set
