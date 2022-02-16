49b1af268be1cb5629bbc59d2e68a94958b499cd
ambroslins <ambros.lins@gmail.com>
2022-02-16 21:47:31 +0100

    improve Map haddock

--- src/Map.hs
+++ src/Map.hs
@@ -5,18 +5,183 @@
 --
 -- This module exports safe functions from "Data.Map.Stict".
 module Map
-  ( module Data.Map.Strict,
+  ( -- * IntMap
+    Map,
+    null,
+    size,
+
+    -- * Construction
+    empty,
+    singleton,
+    fromSet,
+
+    -- ** From Unordered Lists
+    fromList,
+    fromListWith,
+    fromListWithKey,
+
+    -- ** From Ascending Lists
+    fromAscList,
+    fromAscListWith,
+    fromAscListWithKey,
+    fromDistinctAscList,
+
+    -- ** From Descending Lists
+    fromDescList,
+    fromDescListWith,
+    fromDescListWithKey,
+    fromDistinctDescList,
+
+    -- * Query
+    lookup,
+    (!?),
+    findWithDefault,
+    member,
+    notMember,
+
+    -- ** Lookup Ordered
+    lookupLT,
+    lookupGT,
+    lookupLE,
+    lookupGE,
+    lookupMin,
+    lookupMax,
+
+    -- ** Lookup Index
+    lookupIndex,
+
+    -- ** Submap
+    isSubmapOf,
+    isSubmapOfBy,
+    isProperSubmapOf,
+    isProperSubmapOfBy,
+
+    -- ** Disjoint
+    disjoint,
+
+    -- * Insert
+    insert,
+    insertWith,
+    insertWithKey,
+    insertLookupWithKey,
+
+    -- * Delete
+    delete,
+    deleteMin,
+    deleteMax,
+
+    -- * Update
+    adjust,
+    adjustWithKey,
+    update,
+    updateWithKey,
+    updateMin,
+    updateMax,
+    updateMinWithKey,
+    updateMaxWithKey,
+    alter,
+    alterF,
+
+    -- * View
+    minView,
+    maxView,
+    minViewWithKey,
+    maxViewWithKey,
+
+    -- * Combine
+
+    -- ** Union
+    union,
+    unionWith,
+    unionWithKey,
+    unions,
+    unionsWith,
+
+    -- ** Difference
+    difference,
+    (\\),
+    differenceWith,
+    differenceWithKey,
+
+    -- ** Intersection
+    intersection,
+    intersectionWith,
+    intersectionWithKey,
+
+    -- ** Compose
+    compose,
+
+    -- ** Merge
+    mergeWithKey,
+
+    -- * Map
+    map,
+    mapWithKey,
+
+    -- ** Traverse
+    traverseWithKey,
+    traverseMaybeWithKey,
+
+    -- ** Accumulating
+    mapAccum,
+    mapAccumWithKey,
+    mapAccumRWithKey,
+
+    -- ** Map Keys
+    mapKeys,
+    mapKeysWith,
+    mapKeysMonotonic,
+
+    -- ** Filter Map
+    mapMaybe,
+    mapMaybeWithKey,
+    mapEither,
+    mapEitherWithKey,
+
+    -- * Fold
+
+    -- ** Lazy
+    foldr,
+    foldl,
+    foldrWithKey,
+    foldlWithKey,
+    foldMapWithKey,
+
+    -- ** Strict
+    foldr',
+    foldl',
+    foldrWithKey',
+    foldlWithKey',
+
+    -- * Conversions
+    elems,
+    keys,
+    keysSet,
+
+    -- ** Lists
+    toList,
+    assocs,
+    toAscList,
+    toDescList,
+
+    -- * Filter
+    filter,
+    filterWithKey,
+    restrictKeys,
+    withoutKeys,
+
+    -- * Partition
+    partition,
+    partitionWithKey,
+
+    -- * Split
+    take,
+    drop,
+    split,
+    splitAt,
+    splitLookup,
+    splitRoot,
   )
 where
 
-import Data.Map.Strict hiding
-  ( deleteAt,
-    deleteFindMax,
-    deleteFindMin,
-    elemAt,
-    findIndex,
-    findMax,
-    findMin,
-    updateAt,
-    (!),
-  )
+import Data.Map.Strict
