8429884715de74494e944c78d4bcabfc8745943a
ambroslins <ambros.lins@gmail.com>
2022-02-20 00:01:23 +0100

    improve Sequence haddock

--- src/Sequence.hs
+++ src/Sequence.hs
@@ -5,11 +5,116 @@
 --
 -- This module exports safe functions from "Data.Sequence".
 module Sequence
-  ( module Data.Sequence,
+  ( -- * Sequence
+    Seq (..),
+
+    -- * Accessors
+
+    -- ** Length information
+    length,
+    null,
+
+    -- ** Indexing
+    lookup,
+    (!?),
+
+    -- ** Views
+    viewl,
+    ViewL (..),
+    viewr,
+    ViewR (..),
+
+    -- ** Subsequences
+    tails,
+    inits,
+    chunksOf,
+    take,
+    drop,
+    splitAt,
+
+    -- * Construction
+    empty,
+    singleton,
+    (<|),
+    (|>),
+    (><),
+    fromList,
+    fromFunction,
+    fromArray,
+    replicate,
+    replicateA,
+
+    -- ** Iterative Construction
+    iterateN,
+    unfoldr,
+    unfoldl,
+
+    -- * Modifying
+    adjust,
+    adjust',
+    update,
+    insertAt,
+    deleteAt,
+
+    -- * Transformations
+    mapWithIndex,
+    traverseWithIndex,
+    reverse,
+    intersperse,
+
+    -- * Folds
+    foldMapWithIndex,
+    foldlWithIndex,
+    foldrWithIndex,
+
+    -- * Scans
+    scanl,
+    scanr,
+
+    -- * Working with predicates
+
+    -- ** Filter
+    filter,
+
+    -- ** Partition
+    partition,
+    takeWhileL,
+    takeWhileR,
+    dropWhileL,
+    dropWhileR,
+    spanl,
+    spanr,
+    breakl,
+    breakr,
+
+    -- ** Searching
+    elemIndexL,
+    elemIndicesL,
+    elemIndexR,
+    elemIndicesR,
+    findIndexL,
+    findIndicesL,
+    findIndexR,
+    findIndicesR,
+
+    -- * Sorting
+    sort,
+    sortBy,
+    sortOn,
+    unstableSort,
+    unstableSortBy,
+    unstableSortOn,
+
+    -- * Zipping
+    zip,
+    zipWith,
+    zip3,
+    zipWith3,
+    zip4,
+    zipWith4,
+    unzip,
+    unzipWith,
   )
 where
 
-import Data.Sequence hiding
-  ( cycleTaking,
-    index,
-  )
+import Data.Sequence
